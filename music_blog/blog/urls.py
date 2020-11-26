11aa5725ac9b155e907f8d35deb326433974aac7
jaumemy <jaume.montane@outlook.com>
2020-11-26 12:52:48 -0500

    Part 5

--- music_blog/blog/urls.py
+++ music_blog/blog/urls.py
@@ -12,10 +12,10 @@ urlpatterns = [
     path('post/<int:pk>/',views.PostDetailView.as_view(),
         name = 'post_detail'),
 
-    path('post/new',views.CreatePostView.as_view(),
+    path('post/new/',views.CreatePostView.as_view(),
         name = 'post_new'),
 
-    path('post/<int:pk>/edit',views.PostUpdateView.as_view(),
+    path('post/<int:pk>/edit/',views.PostUpdateView.as_view(),
         name = 'post_edit'),
 
     path('post/<int:pk>/remove/',views.PostDeleteView.as_view(),
@@ -24,4 +24,15 @@ urlpatterns = [
     path('drafts/',views.DraftListView.as_view(),
         name = 'post_draft_view'),
 
+    path('post/<int:pk>/comment/',views.add_comment_to_post,
+        name = 'add_comment_to_post'),
+
+    path('comment/<int:pk>/approve/',views.comment_approve,
+        name = 'comment_approve'),
+
+    path('comment/<int:pk>/remove/',views.comment_remove,
+        name = 'comment_remove'),
+
+    path('post/<int:pk>/publish/',views.post_publish,
+        name = 'post_publish'),
 ]
--- music_blog/blog/views.py
+++ music_blog/blog/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-from blog.models import Post, Comment
+from django.shortcuts import render,get_object_or_404,redirect
+from blog.models import Post,Comment
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from blog.forms import PostForm,CommentForm
 from django.urls import reverse_lazy
@@ -46,3 +47,43 @@ class DraftListView(LoginRequiredMixin,ListView):
 
     def get_queryset(self):
         return Post.objects.filter(published_date__isnull=True).order_by('-published_date_daste')
+
+
+#############################################
+#############################################
+
+@login_required
+def post_publish(request,pk):
+    post = get_object_or_404(Post,pk=pk)
+    post.publish
+    return redirect('post_detail',pk=pk)
+
+
+@login_required
+def add_comment_to_post(request,pk):
+    post = get_object_or_404(Post,pk=pk)
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return redirect('post_detail',pk=post.pk)
+    else:
+        form = CommentForm()
+    return render(request,'blog/comment_form.html',{'form':form})
+
+
+@login_required
+def comment_approve(request,pk):
+    comment = get_object_or_404(Comment,pk)
+    comment.approve()
+    return redirect('post_detail',pk=comment.post.pk)
+
+
+@login_required
+def comment_remove(request,pk):
+    comment = get_object_or_404(Comment,pk)
+    post_pk = comment.post.pk
+    comment.delete()
+    return redirect('post_detail',pk=post_pk)
